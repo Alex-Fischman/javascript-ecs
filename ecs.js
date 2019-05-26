7c1290d25d0502f6f7bd02046d0ba2f89ae1b9df
Alex <alex@fischman.org>
2019-05-26 14:46:20 -0700

    Update ecs.js

--- ecs.js
+++ ecs.js
@@ -1,39 +1,39 @@
 class Component {
-	get type() { return this.constructor.name; }
-
 	constructor(data) {
 		this.data = data;
 	}
-}
+} 
 
 class Entity {
-	get type() { return this.constructor.name; }
-
 	constructor(id) {
 		this.id = id;
 		this.components = [];
 	}
-	
-	get(type) { return this.components.find(c => c.type === type); }
 
-	assign(component) {
-		const included = this.get(component.type);
+	type(component) {
+		return component.prototype === undefined?
+			component.constructor.name:
+			component.prototype.constructor.name;
+	}
+	
+	get(component) { return this.components.find(c => this.type(c) === this.type(component)); }
+
+	set(component) {
+		const included = this.get(component);
 		if (included === undefined) this.components.push(component);
 		else this.components[this.components.indexOf(included)] = component;
 	}
 }
 
 class System {
-	get type() { return this.constructor.name; }
-
-	constructor(types, callback) {
-		this.types = types;
+	constructor(components, callback) {
+		this.components = components;
 		this.callback = callback;
 	}
 
 	run(entities) {
 		entities.forEach(e => 
-			this.types.every(t => e.get(t) !== undefined) &&
+			this.components.every(c => e.get(c) !== undefined) &&
 			this.callback(e)
 		);
 	}
